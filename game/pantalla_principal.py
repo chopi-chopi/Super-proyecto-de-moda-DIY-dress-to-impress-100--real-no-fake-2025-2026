bd2fa56b579914b7eeaac81b52c43705881b9cc6
chopi-chopi <sophiamvieira1a31@gmail.com>
2026-01-09 19:55:13 -0400

    ahora si funciona

--- game/pantalla_principal.py
+++ game/pantalla_principal.py
@@ -4,6 +4,7 @@ import importlib.util
 import pygame
 
 pygame.init()
+current_screen = "menu"
 from settings import settings
 running = True
 screen = settings.screen
@@ -17,22 +18,26 @@ MENU_MOUSE_POS = pygame.mouse.get_pos()
 
 # Loop principal de la pantalla de título
 def start_screen():
-    running = True
+    global running
+    global current_screen
     NUEVO_JUEGO = settings.BOTONES(None, (640, 250), "NUEVO JUEGO", font, PINK, LIGHT_GREEN)
     CARGAR_JUEGO = settings.BOTONES(None, (640, 320), "CARGAR JUEGO", font, PINK, LIGHT_GREEN)
     OPCIONES = settings.BOTONES(None, (640, 390), "OPCIONES", font, PINK, LIGHT_GREEN)
-    while running:
+    while running and current_screen == "menu":
         MENU_MOUSE_POS = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN: 
                 if NUEVO_JUEGO.checkForInput(event.pos):
-                 nuevo_juego()
+                 current_screen = "nuevo_juego"
+                 break
                 elif CARGAR_JUEGO.checkForInput(event.pos):
-                 cargar_juego()
+                 current_screen = "cargar_juego"
+                 break
                 elif OPCIONES.checkForInput(event.pos):
-                 opciones()
+                 current_screen = "opciones"
+                 break 
     
         # Render por frame
         screen.fill(LIGHT_GREEN)
@@ -52,30 +57,35 @@ def start_screen():
         pygame.display.flip()
 
 def nuevo_juego():
-    while True:
+    while running and current_screen == "nuevo_juego":
      PLAY_MOUSE_POS = pygame.mouse.get_pos()
      screen.fill("pink")
      screen.blit(font.render("prueba jiji", True, WHITE), (400,300))
 
+     pygame.display.flip()
+
 def cargar_juego():
-    while True:
-       LOAD_MOUSE_POS = pygame.mouse.get_pos()
+    while running and current_screen == "cargar_juego":
+        LOAD_MOUSE_POS = pygame.mouse.get_pos()
+
+        pygame.display.flip()
 
 def opciones():
-    while True: 
+    while running and current_screen == "opciones":
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
         screen.fill("lightblue")
         screen.blit(font.render("opciones", True, WHITE), (500,300))
 
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
-    current_screen = "inicio"
-    while True:
-     if current_screen == 'menu':
-            current_screen = start_screen()
-     elif current_screen == 'nuevo_juego':
-            current_screen = nuevo_juego()
-     elif current_screen == 'cargar':
-            current_screen = cargar_juego()
-     elif current_screen == 'opciones':
-            current_screen = opciones()
+    while running:
+        if current_screen == 'menu':
+            start_screen()
+        elif current_screen == 'nuevo_juego':
+            nuevo_juego()
+        elif current_screen == 'cargar':
+            cargar_juego()
+        elif current_screen == 'opciones':
+            opciones()
