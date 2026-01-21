93b28111ef3840b33c5e857c763506a377f584fc
zizisssss <isabellapinerom@gmail.com>
2026-01-20 21:54:48 -0400

    cambios en ropa-titulos- BOTONES

--- game/cosas que me dan miedo/mytry-isa.py
+++ game/cosas que me dan miedo/mytry-isa.py
@@ -22,6 +22,12 @@ def start_screen():
     base_dir = os.path.dirname(__file__)
     image_path = os.path.normpath(os.path.join(base_dir, "..", "assets", "FONDOS", "fondo principal.png"))
 
+    foto_path = os.path.join(os.path.dirname(__file__), "..", "assets", "NOMBRES", "TITULO V.1.png")
+    titulo_overlay = pygame.image.load(foto_path).convert_alpha()
+    titulo_overlay = pygame.transform.scale(titulo_overlay, (350, 350))
+    TITULO_OVERLAY_RECT = titulo_overlay.get_rect(center=(605, 100))
+
+
     fondo = None
     try:
         fondo = pygame.image.load(image_path).convert()
@@ -51,11 +57,9 @@ def start_screen():
     
         # Render por frame
         screen.blit(fondo, (0, 0))
+        screen.blit(titulo_overlay, TITULO_OVERLAY_RECT)
 
-        # Render del título
-        title_screen = font.render("KISS KISS FALL IN LOVE", True, PINK)
-        title_rect = title_screen.get_rect(center=(screen.get_width() // 2, 80))
-        screen.blit(title_screen, title_rect)
+        
 
         NUEVO_JUEGO.changeColor(MENU_MOUSE_POS)
         NUEVO_JUEGO.update(screen)
@@ -96,26 +100,26 @@ def nuevo_juego():
     cloth_path= cloth_path[:max_botones]
 
     #miniatura de la ropa
-    thumbs = []
+    
     thumb_rects =[]
     posi_w, posi_h = 150, 150
-    gap =16
-    start_x= screen.get_width() - (gap + 100) * max_botones
-    start_y= 40
-    
-    for idx, p in enumerate(cloth_path):
-        surf= pygame.image.load(p).convert_alpha()
-        thumb= pygame.transform.smoothscale(surf, (posi_w, posi_h))
-        x = start_x + idx * (posi_w + gap)
-        y = start_y
-        rect = thumb.get_rect(topleft=(x, y))
+    gap =40
+    start_x= 590
+    start_y= 150
+    for fila in range(2):
+        for columna in range (3):
+            x = start_x + columna * (posi_w + gap)
+            y = start_y + fila * (posi_h + gap)
+            rect = pygame.Rect(x, y, posi_w, posi_h)
+            thumb_rects.append(rect)
+
+    thumbs = []
+    for path in cloth_path:
+        surf = pygame.image.load(path).convert_alpha()
+        thumb = pygame.transform.scale(surf, (posi_w, posi_h))
         thumbs.append(thumb)
-        thumb_rects.append(rect)
 
-
-
-    selection_cloth = None
-    selection_cloth_rect = None
+    
 
     fondo = None
     try:
@@ -126,22 +130,24 @@ def nuevo_juego():
 
 
 
-
+    selection_cloth = None
+    selection_cloth_rect = overlay_rect.copy()
 
 
     while running and current_screen == "nuevo_juego":
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_ESCAPE: 
+                    current_screen = "menu" 
+                    break
             
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
                 for idx, rect in enumerate(thumb_rects):
-                    if rect.collidepoint(event.pos):
+                    if rect.collidepoint(event.pos) and idx < len(cloth_path):
                         path = cloth_path[idx]
                         print("Cargando:", path)
 
@@ -151,20 +157,17 @@ def nuevo_juego():
                             selection_cloth_rect = overlay_rect
                         except Exception as e:
                             print(f"No se pudo cargar la imagen de ropa: {path} -> {e}")
-            
-            
-       
 
-        
-        
+                
         screen.blit(fondo, (0, 0))
         screen.blit(overlay, overlay_rect)
+       
+        for i, rect in enumerate(thumb_rects):
+                    if i < len(thumbs):
+                        screen.blit(thumbs[i], rect)
+                    pygame.draw.rect(screen, (0, 0, 0), rect, 2)
 
  
-        for thumb, rect in zip(thumbs, thumb_rects):
-            screen.blit(thumb, rect)
-            pygame.draw.rect(screen, (0, 0, 0), rect, 2)
-
         if selection_cloth and selection_cloth_rect:
             screen.blit(selection_cloth, selection_cloth_rect)
         
@@ -172,14 +175,7 @@ def nuevo_juego():
 
     
         
-        for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        running = False
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            current_screen = "menu"
-                            break
-
+       
 
 
     fondo = None
